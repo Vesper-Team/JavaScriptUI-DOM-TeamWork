e15dbcdaee32c79b2edd7ae5628a78975e2278a3
kossov <ogi_2112@yahoo.com>
2015-07-24 14:29:04 +0300

    bugfixes- stackedCheckers, fixing setupGameBoard
    removed the magic number 12.3 but added some more in field -> should
    stack checkers :)

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,5 @@
 // CONSTANTS
-//var smallFieldWidth = 46, // pixels
-//    bigFieldWidth = 52,
-var fieldWidth = 54,
+var fieldWidth = 54, // the width of every of the 24 fields
     offsetBetweenFields = 5, // between small field(white) and big field(dark)
     boardSide = 324, // the width of one side of the board
     boardBar = 34, // the thing between the board sides in pixels
@@ -13,9 +11,9 @@ var fieldWidth = 54,
 // GAME ELEMENTS - canvas, ctx, keystate
 var canvas,
     ctx,
-    keystate;
+    clickedField;
 
-// DEFINING GAME OBJECTS - player, checker(backgammon ???), dice, board(not if it is just a background) ...
+// DEFINING GAME OBJECTS - player, checker, dice, board(not if it is just a background) ...
 var player = (function () {
     var player = {
         init: function (name, color) {
@@ -96,7 +94,10 @@ var player = (function () {
     return player;
 }());
 
-var field = function () { // the board is filled with 24 fields
+var field = function () { /* the board is filled with 24 fields,
+ IT IS A RESPONSIBILITY OF THE FIELDS TO GIVE COORDINATES TO THE CHECKERS, WITH RESPECT TO THEIR OWN X,Y COORDINATES
+ IN THIS WAY WE DON'T CARE HOW TO STACK CHECKERS AND TO TAKE INTO ACCOUNT THEIR Xs AND Ys,
+ THE FIELD JUST STACKS THEM DEPENDING ON THE COUNT IT IS HOLDING, GIVING HIS OWN X, Y */
     var field = {
         init: function (x, y) {
             this.checkers = [];
@@ -113,31 +114,24 @@ var field = function () { // the board is filled with 24 fields
             this.checkers.pop();
         },
         draw: function () {
-
-            /* DELETE THIS - MADE ONLY FOR TESTING PURPOSES that 12.3 tho.. */
-            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            //ctx.beginPath();
-            //ctx.fillRect(this.x, this.y, fieldWidth, 50);
-            //ctx.fill();
-            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
             var count = this.checkers.length, // count checkers, if more than 5 draw stacked
                 i,
                 fieldCenter;
             for (i = 0; i < count; i += 1) {
                 if (i > 4) {
-                    // draw stacked - could be function but..
+                    // draw stacked
                     ctx.save();
                     ctx.fillStyle = 'red';
                     ctx.font = "12px Verdana";
-                    ctx.fillText('x' + i - 4, this.x + fieldWidth / 2, this.y + checkerHeight / 2);
+                    ctx.fillText('x' + (i - 3), (this.x-6) + fieldWidth / 2, this.y + checkerRadius + 4);
+                    // x2 for the 6th checker because it represents how many checkers are stacked
+                    // 6 and 4 are just a magic numbers to position the text in the center of the checker, because the text has also its own offset
                     ctx.restore();
                 } else {
-
-                    if (this.y < canvas.height / 2) { // if field is in the upper half of the board, should stack them up-bottom
+                    if (this.y < canvas.height / 2) { // if field is in the upper half of the board, should stack them up to bottom
                         fieldCenter = this.x + (fieldWidth / 2); // x coordinate for the checker center
                         this.checkers[i].draw(fieldCenter, this.y + i * checkerHeight + checkerRadius);
-                    } else { // when the field is in the lower part, should stack them from bottom-up
+                    } else { // when the field is in the lower part, should stack them from bottom to up
                         fieldCenter = this.x + (fieldWidth / 2); // x coordinate for the checker center
                         this.checkers[i].draw(fieldCenter, this.y - i * checkerHeight + checkerRadius)
                     }
@@ -219,10 +213,10 @@ var checker = function () {
 }();
 
 var board = function () {
-    var bottomSideAllSet = false, // flag to indicate that we must start with a field having 5 checkers in it
+    var bottomSideAllSet = false, // flag to indicate that we must start with a field having 5 checkers in it(the 12th field)
     // at bottom we start with  12. 5 3 5 2 .23
     // at top we start with     11. 5 3 5 2 .0
-        patternToPutColors = [];
+        patternToPutColors = []; // colors are inverted in the opposite sides of the field, sorry it truly looks ugly..
     patternToPutColors[0] = 1;
     patternToPutColors[5] = 0;
     patternToPutColors[7] = 0;
@@ -242,51 +236,49 @@ var board = function () {
         }
     };
 
-    function setupHalfGameBoard(self, flag) {
-        // TODO: REFACTURE THIS SHIT, GOOD THING ITS WORKING
+    function setupHalfGameBoard(self, bottomSideAllSet) {
         var i,
             len,
             x,
             y,
             currentField,
-            patternToPutCheckers = [2, 0, 0, 0, 0, 5, 0, 3, 0, 0, 0, 5], // length 12
+            patternOfTheNumberOfCheckersToPut = [2, 0, 0, 0, 0, 5, 0, 3, 0, 0, 0, 5],// on 0 field put 2, on field 1 put 0, etc.. the setup rules..
             j,  // needed to go trough the patternToPutCheckers
             colorToPut,
-            countPassedFields = 0; // when === 6 we must add the borderBar to the X
+            countPassedFields = 0; // needed to add a boardBar offset to the X coordinate
 
-        if (!flag) {
-            i = 0;
-            j = 0;
-        } else {
+        if (bottomSideAllSet) {
             i = 12;
             j = 11;
+        } else {
+            i = 0;
+            j = 0;
         }
         for (len = i + 12; i < len; i += 1, (i >= 12) ? j -= 1 : j += 1, countPassedFields += 1) {
-            if (countPassedFields >= 6) {
-                x = (!flag) * canvas.width - boardFrameOffset - ((countPassedFields + !flag) * fieldWidth) - boardBar;
+            if (countPassedFields >= 6) {  // after every 6 fields we must consider the boardBar also, in both the upper and the lower side of the board
+                x = (!bottomSideAllSet) * canvas.width - boardFrameOffset - ((countPassedFields + !bottomSideAllSet) * fieldWidth) - boardBar;
             } else {
-                x = (!flag) * canvas.width - boardFrameOffset - ((countPassedFields + !flag) * fieldWidth); // we are at bottom !flag = 1
+                x = (!bottomSideAllSet) * canvas.width - boardFrameOffset - ((countPassedFields + !bottomSideAllSet) * fieldWidth);
+                // if we are at the bottom we must consider the canvas width and to subtract from it
             }
+            y = (!bottomSideAllSet) * (canvas.height - checkerHeight) - boardFrameOffset;
 
-            y = (!flag) * (canvas.height / 2 + 12.3 * boardFrameOffset) - boardFrameOffset; //edit this if you want to change the bottom fields take position their so called 0,0 :D
-            // now their '0,0' is the bottom-left corner because of this 12.. there it is used the MASTER NINJA technique called 'nagodigo'
-
-            if (x < 0) {
-                x *= -1;
-            }
-            if (y < 0) {
-                y *= -1;
+            if (x < 0) { // because we are extracting everything from the canvas width or height
+                x *= -1; // when we are at the bottom side, we start from the MOST-RIGHT field(our 0 field) so canvas height, and width must be considered,
+            }            // while when we are at the upper side of the board, the MOST-LEFT field(the 12th field) must be filled first,
+            if (y < 0) { // so canvas width and height in the upper side are 0, and we get a negative number but we actually need that number to be positive,
+                y *= -1; // to go to the right.. to fill the rest of the fields.. it's all because of how the fields position in backgammon
             }
             if (patternToPutColors[j] === 1) {
                 colorToPut = 'white';
-                patternToPutColors[j] = 0;
+                patternToPutColors[j] = 0; // swapping the pattern for the next half of the board
             } else if (patternToPutColors[j] === 0) {
                 colorToPut = 'black';
                 patternToPutColors[j] = 1;
             }
-            currentField = Object.create(field).init(x, y);
-            putCheckersOnField(patternToPutCheckers[j], currentField, colorToPut);
-            self.addField(currentField);
+            currentField = Object.create(field).init(x, y); // creating one of the 24 fields in the board
+            putCheckersOnField(patternOfTheNumberOfCheckersToPut[j], currentField, colorToPut); // assigning checkers to it with respect to the Backgammon setup rules
+            self.addField(currentField); // adding the field to the board, used 'self' because the scope of this has changed
         }
     }
 
@@ -303,17 +295,17 @@ var board = function () {
 // function newGame() or main()
 // create initiate and append game canvas
 // keep track of keyboard presses
-// init() - INITIATE GAME OBJECTS;
-// loop() - GAME LOOP FUNCTION, STARTS THE ANIMATION;
+// INITIATE GAME OBJECTS;
+// GAME LOOP, STARTS THE ANIMATION;
+
 function newGame() {
     canvas = document.getElementById('board-canvas');
     ctx = canvas.getContext('2d');
     canvas.addEventListener('click', findPressedField);
 
-    var gameBoard = Object.create(board).init();
-    gameBoard.fields.forEach(function (field) {
-        field.draw()
-    });
+    // INIT GAME OBJECTS, SETS UP GAME
+    board.init();
+
 
     function findPressedField(event) {
         var x = event.offsetX;
@@ -362,34 +354,38 @@ function newGame() {
                     }
                 }
             }
-            if(typeof foundField === 'undefined') { // when we are going out of the for-in and the condition for the upperSide is not met,
+            if (typeof foundField === 'undefined') { // when we are going out of the for-in and the condition for the upperSide is not met,
                 // because we take the previous field element we could not get to check the last one
-                foundField = gameBoard.fields[len-1];
+                foundField = gameBoard.fields[len - 1];
             }
-            console.log(foundField);
+            clickedField = foundField;
+            console.log(clickedField); // testing purposes
         }
     }
 }
+newGame();
 
+function play() {
+    // GAME LOOP
+    // game logic, updating the fields, players turns, dices etc. Update update update
+    // change fields depending on how the player interact(key presses)
 
-// function loop()
-// update()
-// draw()
-// requestAnimationFrame
+    // calls function update()
 
-// function init()
-// initiate game objects and set start positions
-function init() {
+    // draws every time and if we have changes its ok
+        draw();
+        requestAnimationFrame(play);
 }
-
+play();
 
 // function update()
-// calls game objects .update()
+// maybe it should listen for the clickedField
 
-// function draw ()
-// calls game objects .draw()
-function draw(board) {
+
+function draw() {
+    board.fields.forEach(function (field) {
+        field.draw(); // we redraw every field again
+    });
 }
 
-// main();
 window.onload = newGame();
