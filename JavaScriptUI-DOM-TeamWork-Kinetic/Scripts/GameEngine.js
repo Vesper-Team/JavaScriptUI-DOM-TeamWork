92363ca1bf8c03012e17b753a96796a84afa4473
Boray <boray@abv.bg>
2015-07-26 15:54:51 +0300

    add start() at GameEngine.js and GameEntryPoint.js

--- JavaScriptUI-DOM-TeamWork-Kinetic/Scripts/GameEngine.js
+++ JavaScriptUI-DOM-TeamWork-Kinetic/Scripts/GameEngine.js
@@ -1,6 +1,40 @@
 ﻿
 var GameEngine = ( function () {
+	function start(){
+		var x,
+        y,
+        color,
+        i,
+        j,
+        lengthBoard,
+        lengthField;
 
+    var players = [];
+    players.push( Object.create( GameObjects.Player ).init( 'First', 'white' ) );
+    players.push( Object.create( GameObjects.Player ).init( 'Second', 'black' ) );
 
+    var board = GameObjects.Board.init( players );
 
+    GameDraw.background();
+
+    lengthBoard = board.length;
+
+    for ( i = 0; i < lengthBoard; i += 1 ) {
+        lengthField = board[i].length;
+
+        for ( j = 0; j < lengthField; j += 1 ) {
+            x = i;
+            y = j;
+            color = board[i][j].color;
+
+            GameDraw.createCircle( x, y, color );
+        }
+    }
+
+    GameDraw.playGround();
+	}
+	
+	return{
+		start: start,
+	}
 }() )
--- JavaScriptUI-DOM-TeamWork-Kinetic/Scripts/GameEntryPoint.js
+++ JavaScriptUI-DOM-TeamWork-Kinetic/Scripts/GameEntryPoint.js
@@ -2,37 +2,8 @@
 /// <reference path="GameDraw.js" />
 
 ( function () {
-    var x,
-        y,
-        color,
-        i,
-        j,
-        lengthBoard,
-        lengthField;
+        
+    GameEngine.start();
 
-    var players = [];
-    players.push( Object.create( GameObjects.Player ).init( 'First', 'white' ) );
-    players.push( Object.create( GameObjects.Player ).init( 'Second', 'black' ) );
-
-    var board = GameObjects.Board.init( players );
-
-    GameDraw.background();
-
-    lengthBoard = board.length;
-
-    for ( i = 0; i < lengthBoard; i += 1 ) {
-        lengthField = board[i].length;
-
-        for ( j = 0; j < lengthField; j += 1 ) {
-            x = i;
-            y = j;
-            color = board[i][j].color;
-
-            GameDraw.createCircle( x, y, color );
-        }
-    }
-
-    GameDraw.playGround();
-
-    console.log( 'test' );
+	console.log( 'test' );
 }() )
