c1f3aa9690ee3cefcbb2965f1496f24803036b2d
mr.krustev <mr.krustev1994@gmail.com>
2015-07-27 21:10:02 +0300

    Remove unnecessary functions, variables and properties. Add helper functions for Board init. Add function for moving pieces.

--- JavaScriptUI-DOM-TeamWork-Kinetic/Scripts/GameObjects.js
+++ JavaScriptUI-DOM-TeamWork-Kinetic/Scripts/GameObjects.js
@@ -1,227 +1,161 @@
-﻿
-var GameObjects = ( function () {
+﻿var GameObjects = ( function () {
     var Board,
         Piece,
         Player,
         BoardField,
-        CONSTANTS = {
-
-        };
+        CONSTANTS = {};
 
     // pole ot igralnoto pole
-    BoardField = ( function () {
-        var boardField = Object.create( [] );
+    BoardField = (function () {
+        var boardField = Object.create([]);
 
-        Object.defineProperty( boardField, 'init', {
+        Object.defineProperty(boardField, 'init', {
             value: function () {
-
                 return this;
             }
-        } );
-
-        Object.defineProperty( boardField, 'add', {
-            value: function ( piece ) {
-                this.push( piece );
-
-                return this;
-            }
-        } );
+        });
 
         return boardField;
-    }() );
+    }());
 
     // igralno pole
     Board = ( function () {
-        var board = Object.create( [] );
-        var bagOfpieces = [];
-        var boardLength = 24;
+        var board = Object.create([]);
 
-        Object.defineProperty( board, 'init', {
-            value: function ( players ) {
-                var i;
+        // Inner helper functions.
+        function putBoardFields(self) {
+            var i,
+                boardLength = 24;
 
-                this._players = players;
+            for (i = 0; i < boardLength; i += 1) {
+                self.push(Object.create(BoardField).init());
+            }
+        }
 
-                for ( i = 0; i < boardLength; i += 1 ) {
-                    this.push( Object.create( BoardField ).init() );
-                }
+        function addPiecesToBoard(self, color, numberOfPieces, position) {
+            var pieceNumber, currentPiece;
 
-                // TODO: refactor
-                // TODO: refactor  _pieces
-                for ( var piecenumber = 0; piecenumber < 2; piecenumber += 1 ) {
-                    var currentPiece = this._players[0]._pieces.pop();
+            for (pieceNumber = 0; pieceNumber < numberOfPieces; pieceNumber += 1) {
+                currentPiece = Object.create(Piece).init(color);
+                self[position].push(currentPiece);
+            }
+        }
 
-                    this[0].push( currentPiece );
-                }
+        function putPlayerOnePieces(self) {
+            addPiecesToBoard(self, 'white', 2, 0);
+            addPiecesToBoard(self, 'white', 5, 11);
+            addPiecesToBoard(self, 'white', 3, 16);
+            addPiecesToBoard(self, 'white', 5, 18);
+        }
 
-                for ( var piecenumber = 0; piecenumber < 5; piecenumber += 1 ) {
-                    var currentPiece = this._players[1]._pieces.pop();
+        function putPlayerTwoPieces(self) {
+            addPiecesToBoard(self, 'black', 2, 23);
+            addPiecesToBoard(self, 'black', 5, 12);
+            addPiecesToBoard(self, 'black', 3, 7);
+            addPiecesToBoard(self, 'black', 5, 5);
+        }
 
-                    this[5].push( currentPiece );
-                }
-
-                for ( var piecenumber = 0; piecenumber < 3; piecenumber += 1 ) {
-                    var currentPiece = this._players[1]._pieces.pop();
-
-                    this[7].push( currentPiece );
-                }
-
-                for ( var piecenumber = 0; piecenumber < 5; piecenumber += 1 ) {
-                    var currentPiece = this._players[0]._pieces.pop();
-
-                    this[11].push( currentPiece );
-                }
-
-                for ( var piecenumber = 0; piecenumber < 5; piecenumber += 1 ) {
-                    var currentPiece = this._players[1]._pieces.pop();
-
-                    this[12].push( currentPiece );
-                }
-
-                for ( var piecenumber = 0; piecenumber < 3; piecenumber += 1 ) {
-                    var currentPiece = this._players[0]._pieces.pop();
-
-                    this[16].push( currentPiece );
-                }
-
-                for ( var piecenumber = 0; piecenumber < 5; piecenumber += 1 ) {
-                    var currentPiece = this._players[0]._pieces.pop();
-
-                    this[18].push( currentPiece );
-                }
-
-                for ( var piecenumber = 0; piecenumber < 2; piecenumber += 1 ) {
-                    var currentPiece = this._players[1]._pieces.pop();
-
-                    this[23].push( currentPiece );
-                }
-
+        Object.defineProperty(board, 'init', {
+            value: function () {
+                var self = this;
+                putBoardFields(self);
+                putPlayerOnePieces(self);
+                putPlayerTwoPieces(self);
                 return this;
             }
-        } );
+        });
 
-        Object.defineProperty( board, 'addPiece', {
-            value: function ( piece, nuberOfBoardfield ) {
-
-                this[nuberOfBoardfield].push( piece );
-
+        // Called from update when moving. Ex.: gameBoard.movePiece({color:white}, 2, 5);
+        Object.defineProperty(board, 'movePiece', {
+            value: function (piece, fromBoardField, toBoardField) {
+                this[fromBoardField].pop();
+                this[toBoardField].push(piece);
                 return this;
             }
-        } );
-
-        Object.defineProperty( board, 'removePiece', {
-            value: function ( piece, nuberOfBoardfield ) {
-
-                this[nuberOfBoardfield].pop();
-
-                return this;
-            }
-        } );
-
+        });
 
         return board;
     }() );
 
     // igrach
-    Player = ( function () {
-        var player = Object.create( {} );
-        var CONSTANTS_PLAYER = {
-            TOTAL_NUMBER_OF_PIECES: 15,
-            INIT_X: 0,
-            INIT_Y: 0,
-        }
+    Player = (function () {
+        var player = Object.create({});
+        //var CONSTANTS_PLAYER = {
+        //    TOTAL_NUMBER_OF_PIECES: 15,
+        //    INIT_X: 0,
+        //    INIT_Y: 0
+        //};
 
-        Object.defineProperty( player, 'init', {
-            value: function ( name, color ) {
+        Object.defineProperty(player, 'init', {
+            value: function (name, color) {
                 this.name = name;
                 this.color = color;
-                this.isPlayerTurn = false;
-                this._pieces = [];
-
-                for ( i = 0; i < CONSTANTS_PLAYER.TOTAL_NUMBER_OF_PIECES; i += 1 ) {
-                    this._pieces.push( Object.create( Piece ).init( color ) );
-                }
+                this.isOnTurn = false;
 
                 return this;
             }
-        } );
+        });
 
-        Object.defineProperty( player, 'addPiece', {
-            value: function ( piece ) {
-                this._pieces.push( piece );
-
-                return this;
-            }
-        } );
-
-        Object.defineProperty( player, 'removePiece', {
-            value: function ( piece ) {
-                this._pieces.pop();
-
-                return this;
-            }
-        } );
-
-        Object.defineProperty( player, 'name', {
+        Object.defineProperty(player, 'name', {
             get: function () {
                 return this._name;
             },
-            set: function ( value ) {
+            set: function (value) {
                 this._name = value;
             }
-        } );
+        });
 
-        Object.defineProperty( player, 'color', {
+        Object.defineProperty(player, 'color', {
             get: function () {
                 return this._color;
             },
-            set: function ( value ) {
+            set: function (value) {
                 this._color = value;
             }
-        } );
+        });
 
-        Object.defineProperty( player, 'isPlayerTurn', {
+        Object.defineProperty(player, 'isOnTurn', {
             get: function () {
-                return this._isPlayerTurn;
+                return this._isOnTurn;
             },
-            set: function ( value ) {
-                this._isPlayerTurn = value;
+            set: function (value) {
+                this._isOnTurn = value;
             }
-        } );
+        });
 
         return player;
-    }() );
+    }());
 
     // pulowe
     Piece = ( function () {
-        var piece = Object.create( {} );
+        var piece = Object.create({});
 
-        Object.defineProperty( piece, 'init', {
-            value: function ( color ) {
+        Object.defineProperty(piece, 'init', {
+            value: function (color) {
                 this.color = color;
                 this.isChosen = false;
 
                 return this;
             }
-        } );
+        });
 
-        Object.defineProperty( piece, 'color', {
+        Object.defineProperty(piece, 'color', {
             get: function () {
                 return this._color;
             },
-            set: function ( value ) {
+            set: function (value) {
                 this._color = value;
             }
-        } );
+        });
 
-        Object.defineProperty( piece, 'isChosen', {
+        Object.defineProperty(piece, 'isChosen', {
             get: function () {
                 return this._isChosen;
             },
-            set: function ( value ) {
+            set: function (value) {
                 this._isChosen = value;
             }
-        } );
+        });
 
         return piece;
     }() );
@@ -230,8 +164,8 @@ var GameObjects = ( function () {
     return {
         Board: Board,
         Player: Player,
-        Piece: Piece,
-    }
+        Piece: Piece
+    };
 
-}() )
+}());
 
