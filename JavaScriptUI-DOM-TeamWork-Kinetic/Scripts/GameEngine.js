0f9b5317e5a9ce68372d62c814dfbd260b748169
mr.krustev <mr.krustev1994@gmail.com>
2015-07-27 13:03:46 +0300

    Add pseudo code for game update according to game state.

--- JavaScriptUI-DOM-TeamWork-Kinetic/Scripts/GameEngine.js
+++ JavaScriptUI-DOM-TeamWork-Kinetic/Scripts/GameEngine.js
@@ -1,40 +1,62 @@
-﻿
-var GameEngine = ( function () {
-	function start(){
-		var x,
-        y,
-        color,
-        i,
-        j,
-        lengthBoard,
-        lengthField;
+﻿var GameEngine = ( function () {
+    function start() {
+        var x,
+            y,
+            color,
+            i,
+            j,
+            lengthBoard,
+            lengthField;
 
-    var players = [];
-    players.push( Object.create( GameObjects.Player ).init( 'First', 'white' ) );
-    players.push( Object.create( GameObjects.Player ).init( 'Second', 'black' ) );
+        var players = [];
+        players.push(Object.create(GameObjects.Player).init('First', 'white'));
+        players.push(Object.create(GameObjects.Player).init('Second', 'black'));
 
-    var board = GameObjects.Board.init( players );
+        var board = GameObjects.Board.init(players);
 
-    GameDraw.background();
+        GameDraw.background();
 
-    lengthBoard = board.length;
+        lengthBoard = board.length;
 
-    for ( i = 0; i < lengthBoard; i += 1 ) {
-        lengthField = board[i].length;
+        for (i = 0; i < lengthBoard; i += 1) {
+            lengthField = board[i].length;
 
-        for ( j = 0; j < lengthField; j += 1 ) {
-            x = i;
-            y = j;
-            color = board[i][j].color;
+            for (j = 0; j < lengthField; j += 1) {
+                x = i;
+                y = j;
+                color = board[i][j].color;
 
-            GameDraw.createCircle( x, y, color );
+                GameDraw.createCircle(x, y, color);
+            }
         }
+
+        GameDraw.playGround();
     }
 
-    GameDraw.playGround();
-	}
-	
-	return{
-		start: start,
-	}
-}() )
+    function update(){
+        // currentPlayer = GetCurrentPlayer - depending on player.isOnTurn or isFirstPlayerOnTurn
+
+        // flag hasThrownDice -> if not - throw dice(allowedMoves = diceResult, currentPlayerMoves = 0); else - continue
+
+        // if (currentPlayer.hasHitPiece) -> Call function(s) to deal with this situation.
+            // if can't put piece -> playerMoves = allowedMoves
+
+        // if ((currentPlayerMoves < allowedMoves) && hasMovedPiece (sets to true when called from onDrag event on piece)
+
+            // Subcases: move from position to position/ collect piece
+            // Call function(s) to deal with this situation. -> hasMovedPiece = false, currentPlayerMoves++;
+
+        // Missed logic?
+
+        // if current player has no pieces on the board -> He wins.
+
+        // if (playerMoves === allowedMoves) -> change player, hasThrownDice = false
+    }
+
+    return {
+        start: start,
+        update: update
+    };
+}());
+
+// All events will call GameEngine.Update() and GameDraw.Update().
