ec9f067d0fc30a26ee4835d889f12995d909612e
kiko81 <k.kolev@tobo-bg.com>
2015-07-28 12:53:31 +0300

    Minor fix on game start - reference to array in field added instead of field itself

--- JavaScriptUI-DOM-TeamWork-Kinetic/Scripts/GameEngine.js
+++ JavaScriptUI-DOM-TeamWork-Kinetic/Scripts/GameEngine.js
@@ -1,4 +1,6 @@
 ﻿var GameEngine = ( function () {
+    var board;
+    
     function start() {
         var x,
             y,
@@ -12,7 +14,7 @@
         players.push(Object.create(GameObjects.Player).init('First', 'white'));
         players.push(Object.create(GameObjects.Player).init('Second', 'black'));
 
-        var board = GameObjects.Board.init(players);
+        board = GameObjects.Board.init(players);
         //console.log(board);
 
         GameDraw.background();
@@ -21,17 +23,33 @@
         //console.log(lengthBoard)
 
 
-        for (i = 1; i < lengthBoard - 1; i += 1) {
-            lengthField = board[i].pieces.length;
-        console.log(board[i])
+        // for (i = 1; i < lengthBoard - 1; i += 1) {
+        //     lengthField = board[i].pieces.length;
+        // console.log(board[i])
 
 
-            for (j = 0; j < lengthField; j += 1) {
-                x = i;
-                y = j;
-                color = board[i].pieces[j].color;
+        //     for (j = 0; j < lengthField; j += 1) {
+        //         x = i;
+        //         y = j;
+        //         color = board[i].pieces[j].color;
+
+        //         GameDraw.createCircle(x, y, color);
+        //     }
+        // }
+        
+        for (x = 0; x < lengthBoard; x += 1) {
+            lengthField = board[x].pieces.length;                       
+
+            for (y = 0; y < lengthField; y += 1) {               
+                color = board[x].pieces[y].color;
 
                 GameDraw.createCircle(x, y, color);
+            }
+
+            if (x < 13 ) {
+                GameDraw.createRectangleListener( x, 4 );
+            } else {
+                GameDraw.createRectangleListener( x, 0 );
             }
         }
 
@@ -57,10 +75,40 @@
 
         // if (playerMoves === allowedMoves) -> change player, hasThrownDice = false
     }
+    
+    /////////////
+        function updatePlayGround() {
+        var x,
+            y,
+            lengthBoard = board.length,
+            lengthField;
+
+        for ( x = 0; x < lengthBoard; x += 1 ) {
+            lengthField = board[x].length;
+
+            for ( y = 0; y < lengthField; y += 1 ) {
+                color = board[x][y].color;
+
+                GameDraw.createCircle( x, y, color );
+            }
+        }
+
+        GameDraw.updatePlayGround();
+    }
+   
+    function test(x, y) {
+        //alert( x + ' ' + y );
+       
+        board.movePiece( 6, 7 );
+       
+        updatePlayGround();        
+    }
+    /////////////
 
     return {
         start: start,
-        update: update
+        update: update,
+        test: test,
     };
 }());
 
